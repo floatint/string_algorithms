6e948013da91797638c1d96965f33e5594f7b785
Klishin Ilya <klishin-ilya@mail.ru>
2021-02-17 11:13:53 +0300

    SimpleStringBorderFinder update

--- src/main/java/borders/SimpleStringBorderFinder.java
+++ src/main/java/borders/SimpleStringBorderFinder.java
@@ -2,15 +2,17 @@ package borders;
 
 public class SimpleStringBorderFinder {
     String findBorder(String s) {
-        // индекс конца границы
+        // индекс конца границы (с начала строки)
         int border_index = 0;
 
         // идем по строке с конца
         for (int i = s.length() - 1; border_index == 0 && i > 0; --i){
             // пытаемся найти наибольшую границу сдвигая указатель в начале
             int j = 0;
-            while (j < i && s.charAt(j) == s.charAt(s.length() - i + j)){
-                j++;
+            for (j = 0; j < i; j++){
+                if (s.charAt(j) != s.charAt(s.length() - i + j)){
+                    break;
+                }
             }
 
             // если с начала и с конца пройдено одинаковое расстояние, то граница найдена
